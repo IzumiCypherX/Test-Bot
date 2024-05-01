ec7d9297872e4c39b4bea67751d64ef827c2d674
Sidhartha Rao <sidhartharao1@gmail.com>
2024-05-02 00:22:25 +0530

    changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'Hello {update.effective_user.first_name} and {os.environ.get("GREET")}')
 
 
 app = ApplicationBuilder().token("7088882913:AAHazPIPjVaZTmapVcwrztJP2kZAb3XwFWQ").build()
